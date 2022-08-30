0f9a0f675585d0d42f6e55600c137d7e347e9bd5
unknown <yanceevillanueva16@gmail.com>
2022-08-30 20:46:36 +0800

    mainpage

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 import {AppRegistry} from 'react-native';
 import App from './App';
+import Main from './src/features/main/main.page';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Main);
--- src/features/main/main.page.js
+++ src/features/main/main.page.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React from 'react';
+import type {Node} from 'react';
+import {
+    Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
 
-export function Main(){
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+export function Main (){
     return(
-        <div>
-            
-        </div>
+        <Image
+        style={styles.tinyLogo}
+        source={require('assets/logo_no_name.png')}
+      />
+
     );
 }
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default Main;
