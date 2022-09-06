134e6ae6404ca6daec7f21bcbceeac56ee517425
unknown <yanceevillanueva16@gmail.com>
2022-09-06 22:12:06 +0800

    fix index

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import App from './App';
 import Main from './src/features/main/main.page';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => Main);
+AppRegistry.registerComponent(appName, () => App);
